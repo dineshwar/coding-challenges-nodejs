9d9df4148fc3b04f112246fb12cefdfca4afd793
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-04-28 16:19:26 +0530

    Step 1 check if the file are vaild

--- challenge-json-parser/json_parser.js
+++ challenge-json-parser/json_parser.js
@@ -6,7 +6,15 @@ const [fileName] = argv;
 
 async function readFileData(file) {
   fs.readFile(file, "utf8").then((data) => {
-    console.log(data);
+    let ans = 1;
+    const dataLen = data.length;
+    if (!dataLen) {
+      ans = 0;
+    }
+    if (data[0] !== "{" && data[dataLen - 1] !== "}") {
+      ans = 0;
+    }
+    console.log(ans);
   });
 }
 
