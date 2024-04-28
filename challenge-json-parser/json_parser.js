318a486df4f25f366824a14b611bf8fe5791aa18
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-04-28 15:58:14 +0530

    JSON read file

--- challenge-json-parser/json_parser.js
+++ challenge-json-parser/json_parser.js
@@ -2,6 +2,12 @@ const fs = require("node:fs").promises;
 const readline = require("node:readline");
 
 const argv = process.argv.slice(2);
+const [fileName] = argv;
 
+async function readFileData(file) {
+  fs.readFile(file, "utf8").then((data) => {
+    console.log(data);
+  });
+}
 
-
+readFileData(fileName);
